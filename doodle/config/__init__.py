ccb6f0a5475b221d2d760370d2d462d09807c4bf
keakon <keakon@gmail.com>
2016-02-13 22:29:16 +0800

    add logging for config

--- doodle/config/__init__.py
+++ doodle/config/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import logging
 import os
 
 
@@ -7,12 +8,13 @@ doodle_env = os.getenv('DOODLE_ENV')
 try:
     if doodle_env == 'PRODUCTION':
         from .production import ProductionConfig as CONFIG
+        logging.info('Production config loaded.')
     elif doodle_env == 'TEST':
         from .test import TestConfig as CONFIG
+        logging.info('Test config loaded.')
     else:
         from .development import DevelopmentConfig as CONFIG
+        logging.info('Development config loaded.')
 except ImportError:
-    import logging
     logging.warning('Loading config for %s environment failed, use default config instead.', doodle_env or 'unspecified')
-
     from .default import Config as CONFIG
