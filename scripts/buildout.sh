f2b3371780021cb7e0df3c461f05b679ec6c9a1b
keakon <keakon@gmail.com>
2016-02-15 21:54:04 +0800

    add buildout.sh

---
+++ scripts/buildout.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"/..
+if [ ! -f bin/buildout ]; then
+  wget https://bootstrap.pypa.io/bootstrap-buildout.py -O bootstrap-buildout.py && python bootstrap-buildout.py --setuptools-to-dir downloads
+fi
+bin/buildout -N
