4615a88914e12394d2b31e50578717b2d361ea8e
keakon <keakon@gmail.com>
2016-02-18 09:37:40 +0800

    native load js instead of using jquery

--- doodle/static/theme/null/js/append.js
+++ doodle/static/theme/null/js/append.js
@@ -12,37 +12,51 @@
 	if (session_time) {
 		url += '&time=' + session_time[2];
 	}
-	$.ajaxPrefilter('script', function(options) {
-		options.cache = true;
-	});
 	$.ajax({
 		'url': url,
 		'dataType': 'json',
-		'success': function(json) {
-			if (json.user_id) {
-				var append_links = '<li><a href="' + json.logout_url + '">登出</a></li>';
-				if (json.is_admin) {
-					append_links += '<li><a href="' + json.admin_url + '">管理博客</a></li>';
+		'success': function(data) {
+			if (data.user_id) {
+				var append_links = '<li><a href="' + data.logout_url + '">登出</a></li>';
+				if (data.is_admin) {
+					append_links += '<li><a href="' + data.admin_url + '">管理博客</a></li>';
 				}
 				if (ga_id) {
-					ga('set', 'userId', json.user_id);
+					ga('set', 'userId', data.user_id);
 				}
 			} else {
-				append_links = '<li><a href="' + json.login_url + '">登录</a></li>';
+				append_links = '<li><a href="' + data.login_url + '">登录</a></li>';
 			}
 			$('#nav').append(append_links);
-			if (json.append_js_urls) {
-				var js_urls = json.append_js_urls;
-				var scripts = [];
-				for (var i = 0; i < js_urls.length; ++i) {
-					scripts.push('<script src="' + js_urls[i] + '"></script>')
+			if (data.append_js_urls) {
+				var js_urls = data.append_js_urls;
+				var js_count = js_urls.length;
+				var loaded_js = 0;
+				var on_js_load = function() {
+					loaded_js += 1;
+					if (loaded_js == js_count) {
+						if (page == 'article') {
+							$.show_comment_form(data.user_name, data.comment_url_prefix ? data.comment_url_prefix + article_id : '', data.logout_url, data.profile_url, data.login_url);
+						}
+					}
+				};
+				var on_js_ready_state_change = function() {
+					if (this.readyState === "complete") {
+						on_js_load();
+					}
+				};
+				for (var i = 0; i < js_count; ++i) {
+					var script = document.createElement('script');
+					script.async = 1;
+					script.src = js_urls[i];
+					script.onload = on_js_load;
+					script.onreadystatechange = on_js_ready_state_change;
+					document.body.appendChild(script);
 				}
-				$('head').append(scripts.join(''));
 			}
 			if (page == 'article') {
-				show_comment_form(json.user_name, json.comment_url_prefix ? json.comment_url_prefix + article_id : '', json.logout_url, json.profile_url, json.login_url);
-				if (json.is_admin) {
-					$('.post-data').append('<span id="post-operation"><a href="' + json.edit_url_prefix + article_id + '/edit">[编辑]</a></span>');
+				if (data.is_admin) {
+					$('.post-data').append('<span id="post-operation"><a href="' + data.edit_url_prefix + article_id + '/edit">[编辑]</a></span>');
 				}
 			}
 		},
