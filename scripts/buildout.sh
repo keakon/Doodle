e0cb4e76f353fde4827d9264f043a6e445c3bdbe
keakon <keakon@gmail.com>
2016-02-29 13:05:42 +0800

    fix buildout.sh fix #3

--- scripts/buildout.sh
+++ scripts/buildout.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"/..
 if [ ! -f bin/buildout ]; then
-  mkdir download -p
+  mkdir downloads -p
   wget https://bootstrap.pypa.io/bootstrap-buildout.py -O bootstrap-buildout.py && python bootstrap-buildout.py --setuptools-to-dir downloads
 fi
 bin/buildout -N
