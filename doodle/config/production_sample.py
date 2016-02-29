1935c59bad7d508a2cd37e3b7acabe1e0f4e90fe
keakon <keakon@gmail.com>
2016-03-01 00:14:20 +0800

    check https adjust referer fix robots.txt add comments for config

--- doodle/config/production_sample.py
+++ doodle/config/production_sample.py
@@ -6,7 +6,8 @@ from .default import Config
 class ProductionConfig(Config):
     # application config
     DEBUG_MODE = False
-    GZIP = False  # 应该用 nginx 来压缩
+    GZIP = False
+    XHEADERS = True
 
     # redis config
     REDIS_MAIN_DB = {'unix_socket_path': '/tmp/doodle_redis_main.sock'}
