d631613e8d37ee7990dbed7efed8b1d17893e3e0
keakon <keakon@gmail.com>
2016-03-06 02:42:05 +0800

    fix fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from fabric.api import cd, env, local, task, execute, run, runs_once
+from fabric.api import cd, env, lcd, local, run, task
 
 
 env.use_ssh_config = True
 
 
 @task
-def get_current_branch():
+def get_code_branch():
     output = local('git branch', capture=True)
     for line in output.splitlines():
         if line[:2] == '* ':
@@ -16,9 +16,19 @@ def get_current_branch():
 
 
 @task
+def get_config_branch():
+    with lcd('private'):
+        output = local('git branch', capture=True)
+        for line in output.splitlines():
+            if line[:2] == '* ':
+                return line[2:]
+    return 'master'
+
+
+@task
 def update_code(branch='master'):
     with cd('/data/doodle'):
-        run('git fetch -all')
+        run('git fetch --all')
         run('git checkout ' + branch)
         run('git reset --hard origin/' + branch)
 
@@ -26,7 +36,7 @@ def update_code(branch='master'):
 @task
 def update_config(branch='master'):
     with cd('/data/doodle/private'):
-        run('git fetch -all')
+        run('git fetch --all')
         run('git checkout ' + branch)
         run('git reset --hard origin/' + branch)
 
@@ -38,14 +48,17 @@ def restart():
 
 @task(default=True)
 def fast_deploy():
-    branch = get_current_branch()
-    update_code(branch)
+    code_branch = get_code_branch()
+    print('deploying %s code branch' % code_branch)
+    update_code(code_branch)
     restart()
 
 
-@task(default=True)
+@task()
 def deploy():
-    branch = get_current_branch()
-    update_code(branch)
-    update_config(branch)
+    code_branch = get_code_branch()
+    config_branch = get_config_branch()
+    print('deploying %s code branch and %s config branch' % (code_branch, config_branch))
+    update_code(code_branch)
+    update_config(config_branch)
     restart()
